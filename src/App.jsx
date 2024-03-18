f9b329fdff2ed9159b0d5b4bac85ac2f771a1023
kryvenkosasha <143883449+kryvenkosasha@users.noreply.github.com>
2024-03-18 21:43:21 +0100

    is done

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Profile from "./components/Profile/Profile.jsx";
+import userData from "./userData.json";
+import FriendList from "./components/FriendList/FriendList.jsx";
+import friends from "./friends.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx";
+import transactions from "./transactions.json";
+
+function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
+
+export default App;
